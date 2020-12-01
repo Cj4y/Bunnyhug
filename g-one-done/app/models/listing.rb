9c297f7fd1934aad11cd046292c5ba0abbebfee3
cj4y <cjtollola@gmail.com>
2020-12-01 09:31:56 +0100

    add stylesheet asset and price, image and emptiness unit tests

--- g-one-done/app/models/listing.rb
+++ g-one-done/app/models/listing.rb
@@ -1,2 +1,9 @@
 class Listing < ApplicationRecord
+    validates :title, :category, :details, :photos, :colour, presence: true
+    validates :title, uniqueness: true
+    validates :photos, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\z}i,
+    message: 'URL for GIF, JPG or PNG image required.'
+    }
+    validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 end
--- g-one-done/test/models/listing_test.rb
+++ g-one-done/test/models/listing_test.rb
@@ -1,7 +1,58 @@
 require 'test_helper'
 
 class ListingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "listing attributes must not be empty" do
+    listing = Listing.new
+    assert listing.invalid?
+    assert listing.errors[:title].any?
+    assert listing.errors[:category].any?
+    assert listing.errors[:details].any?
+    assert listing.errors[:photos].any?
+    assert listing.errors[:price].any?
+    assert listing.errors[:colour].any?
+    end
+    
+    test "listing price must be positive" do
+      listing = Listing.new(title: "My item ",
+      category: "dress",
+      details: "yyy",
+      photos: "zzz.jpg",
+      colour: "blue") 
+      listing.price = -1
+      assert listing.invalid?
+      assert_equal ["must be greater than or equal to 0.01"],
+      listing.errors[:price]
+      listing.price = 0
+      assert listing.invalid?
+      assert_equal ["must be greater than or equal to 0.01"],
+      listing.errors[:price]
+      listing.price = 1
+      assert listing.valid?
+    end
+    
+    def new_listing(photos)
+      Listing.new(title: "My item title",
+      category: "boots",
+      details: "yyy",
+      colour: "red",
+      price: 1,
+      photos: photos)
+    end
+
+    test "photos" do
+      ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+      http://a.b.c/x/y/z/fred.gif }
+
+      bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+      ok.each do |photos|
+      assert new_listing(photos).valid?,
+      "#{photos} shouldn't be invalid"
+      end
+
+      bad.each do |photos|
+      assert new_listing(photos).invalid?,
+      "#{photos} shouldn't be valid"
+      end
+    end
 end
