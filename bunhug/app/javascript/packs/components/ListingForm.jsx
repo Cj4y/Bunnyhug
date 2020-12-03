748b62d551b638c4feba653141c50c954cc8a322
cj4y <cjtollola@gmail.com>
2020-12-03 10:58:45 +0100

    add listing, pass csrf-token

--- bunhug/app/javascript/packs/components/ListingForm.jsx
+++ bunhug/app/javascript/packs/components/ListingForm.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import axios from 'axios'
+import setAxiosHeaders from "./AxiosHeaders";
 class ListingForm extends React.Component {
   constructor(props) {
     super(props)
