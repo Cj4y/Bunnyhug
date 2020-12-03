3bbc8d013043b3f1635f267b321f99978f4d7b15
cj4y <cjtollola@gmail.com>
2020-12-03 10:56:27 +0100

    add listingForm and state error message

---
+++ bunhug/app/javascript/packs/components/ListingForm.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import axios from 'axios'
+class ListingForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.titleRef = React.createRef()
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    axios
+      .post('/api/v1/listings', {
+        listing: {
+          title: this.titleRef.current.value, //create to access input data
+          active: false, //user will not add an inactive listing 
+        },
+      })
+      .then(response => {
+        const listing = response.data
+        this.props.createListing(listing)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    e.target.reset()
+  }
+
+  render() {
+    return (
+        //try to subit before create
+      <form onSubmit={this.handleSubmit} className="my-3">
+        <div className="form-row">
+          <div className="form-group col-md-8">
+            <input
+              type="text"
+              name="title"
+              ref={this.titleRef}
+              required
+              className="form-control"
+              id="title"
+              placeholder="Listing title here..."
+            />
+          </div>
+          <div className="form-group col-md-4">
+            <button className="btn btn-outline-success btn-block">
+              Add Listing
+            </button>
+          </div>
+        </div>
+      </form>
+    )
+  }
+}
+
+export default ListingForm
+
+ListingForm.propTypes = {
+  createListing: PropTypes.func.isRequired,
+}
