b8bb1fe37781881f5ea0e627726d596be71c5983
cj4y <cjtollola@gmail.com>
2020-12-03 03:30:14 +0100

    install bootstrap, delete user scaffold

--- bunhug/config/routes.rb
+++ bunhug/config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   resources :accounts
   resources :orders
   resources :listings
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 get 'home/index'
 root 'home#index'
