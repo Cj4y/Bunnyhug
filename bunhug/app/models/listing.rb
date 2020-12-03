24f2446a6dee9efd7dde48a04f9e55d55d33a33d
cj4y <cjtollola@gmail.com>
2020-12-03 06:55:31 +0100

    add listing class

---
+++ bunhug/app/models/listing.rb
@@ -0,0 +1,10 @@
+class Listing < ApplicationRecord
+  belongs_to :user
+  validates :title, :category, :details, :photos, :colour, presence: true
+  #validates :title, uniqueness: true
+  validates :photos, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\z}i,
+    message: 'URL for GIF, JPG or PNG image required.'
+  }
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+end
--- bunhug/config/routes.rb
+++ bunhug/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  authenticated :user do
+    root "pages#listing_display", as: :authenticated_root
+  end
   root 'pages#home'
-  get 'pages/listing_display'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
