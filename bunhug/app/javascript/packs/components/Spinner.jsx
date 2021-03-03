d597d67bf46e549eda78d13a5089c7574348c030
cj4y <cjtollola@gmail.com>
2021-03-03 07:44:52 +0100

    add material ui

--- bunhug/app/javascript/packs/components/Spinner.jsx
+++ bunhug/app/javascript/packs/components/Spinner.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 const Spinner = () => {
   return (
-    <div className="d-flex align-items-center justify-content-center py-5">
+    <div z="d-flex align-items-center justify-content-center py-5">
       <div className="spinner-border" role="status">
         <span className="sr-only">Loading...</span>
       </div>
