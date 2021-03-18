bad53399ddebc7eefac22a1a7b83c3d33e6e9aea
cj4y <cjtollola@gmail.com>
2021-03-18 09:12:29 +0100

    change routing and remove authorized check temporarily

--- app/controllers/api/v1/item_galleries_controller.rb
+++ app/controllers/api/v1/item_galleries_controller.rb
@@ -1,14 +1,11 @@
 class Api::V1::ItemGalleriesController < ApplicationController
 
-    def index
-        @galleries = Display.all
-        render json: @galleries
-    end
-
     def show
-        @galleries = ItemGallery.all
-        render json: @galleries
+        @listings = Listing.all
+        @galleries = Display.all
         
+        respond_to do |format|
+            format.heml
+            format.json{render(json: @listings)}
+        end
     end
-
-end
--- app/controllers/api/v1/listings_controller.rb
+++ app/controllers/api/v1/listings_controller.rb
@@ -13,15 +13,15 @@ class Api::V1::ListingsController < ApplicationController
   end
 
   def show
-    if authorized?
+    #if authorized?
       render(
         partial: "listings/add_listing",
         locals: {listing: @listing, user: current_user}
       )
-    end
-    else
-      handle_unauthorized
-    end
+    #end
+    #else
+      #handle_unauthorized
+    #end
   end    
 
   # GET /listings/new
@@ -40,7 +40,7 @@ class Api::V1::ListingsController < ApplicationController
     #@listing= current_user.listings.build(listing_params)
     @listing= Listing.new(listing_params)
 
-    if authorized?
+    #if authorized?
       respond_to do |format|
         if @listing.save
           format.json { render :show, status: :created, location: api_v1_my_listing_path(@listing) }
@@ -50,17 +50,17 @@ class Api::V1::ListingsController < ApplicationController
           format.json do  
             render( json: @listing.errors, status: :unprocessable_entity )
         end
-      end
-    else
-      handle_unauthorized
-    end
+     # end
+    #else
+     # handle_unauthorized
+    #end
     
   end
 
   # PATCH/PUT /listings/1
   # PATCH/PUT /listings/1.json
   def update
-    if authorized?
+    #if authorized?
       respond_to do |format|
         if @listing.update(listing_params)
           format.turbo_stream {}
@@ -77,10 +77,10 @@ class Api::V1::ListingsController < ApplicationController
           format.json do
              render( json: @listing.errors, status: :unprocessable_entity )
         end
-      end
-    else
-      handle_unauthorized
-    end
+     # end
+    #else
+      #handle_unauthorized
+    #end
   end
 
 # Callbacks to share common setup or constraints bt actions
@@ -89,33 +89,31 @@ private def set_listing
 end
 
   def destroy
-    if authorized?
+    #if authorized?
       @listing.destroy
       respond_to do |format|
         format.json { head :no_content }
-      end
-    else
-      handle_unauthorized
-    end
+     # end
+    #else
+     # handle_unauthorized
+    #end
   end
 
-  private
-    
-    def set_listing
+  private def set_listing
       @listing= Listing.find(params[:id])
     end
 
-    def authorized?
-       @listing.user == current_user
-    end
+   # def authorized?
+    #   @listing.user == current_user
+    #end
 
-    def handle_unauthorized
-      unless authorized?
-        respond_to do |format|
-          format.json { render :unauthorized, status: 401 }
-        end
-      end
-    end
+    #def handle_unauthorized
+      #unless authorized?
+       # respond_to do |format|
+        #  format.json { render :unauthorized, status: 401 }
+        #end
+      #end
+    #end
 
     def listing_params
       #:id, :user_id, :title, :category, :image_url, :details, :active, :created_at, :updated_at
