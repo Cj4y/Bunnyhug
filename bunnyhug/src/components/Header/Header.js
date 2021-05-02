2d9ef22de9ecba6eec62c3122c9ffebc48cc370c
cj4y <cjtollola@gmail.com>
2021-05-02 23:19:15 +0200

    logo changes

--- bunnyhug/src/components/Header/Header.js
+++ bunnyhug/src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.scss';
-import logo from '../../assets/logo.png.gif';
+import logo from '../../assets/cover.png';
 
 const Header = props =>{
     return (
@@ -8,7 +8,7 @@ const Header = props =>{
             <div className="wrapper">
                 <div className="logo">
                     <img src={logo} alt="Bunnyhug lgoo"/>
-                    
+
                 </div>
 
 
